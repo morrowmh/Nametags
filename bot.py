5241366d43bed4d2558fb92f51f20a61722adaf5
Michael Morrow <michaelhmorrow98@gmail.com>
2024-08-05 13:54:38 -0400

    update bot.py;.gitignore;requirements.txt

--- bot.py
+++ bot.py
@@ -1,1 +1,12 @@
+import discord
+import os
+from dotenv import load_dotenv
 
+bot = discord.Bot()
+
+def main() -> None:
+    load_dotenv()
+    bot.run(os.getenv("TOKEN"))
+
+if __name__ == "__main__":
+    main()
